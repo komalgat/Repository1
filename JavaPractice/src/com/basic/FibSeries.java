b70d4da06c08bd88c5c33d898ed3a772c75060af
komal gat <gatkomal2714@gmail.com>
2022-12-19 06:26:49 -0800

    fib1
    fib done

---
+++ JavaPractice/src/com/basic/FibSeries.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class FibSeries {
+public static void main(String args[]) {
+	int n1=0,n2=1;
+	int c;
+	System.out.println(n1+" "+n2);
+	for(int i=0;i<=10;i++) {
+	c=n1+n2;
+	System.out.println(" "+c);
+	n1=n2;
+	n2=c;
+	
+}
+}
+}
